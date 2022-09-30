53eeb4e49901964e926a6ec38239905b37c01ec3
Marcell Munnich <marcellmunnich@gmail.com>
2022-09-30 13:02:59 +1000

    Added token validation route

--- index.js
+++ index.js
@@ -87,6 +87,23 @@ app.get('/api/current-user', async (req, res) => {
 	}
 });
 
+app.get('/api/validate-user', async (req, res) => {
+	const token = req.headers['x-access-token'];
+
+	try {
+		const decoded = jwt.verify(token, 'secret123');
+		const email = decoded.email;
+		const user = await User.findOne({ email: email });
+
+		res.status(200);
+		return res.json({ status: 'ok' });
+	} catch (error) {
+		// console.log(error);
+		res.status(403);
+		res.json({ status: 'error', error: 'invalid token' });
+	}
+});
+
 // ------------------------- END OF ROUTES ------------------------------
 
 app.listen(1337, () => {
